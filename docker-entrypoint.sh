6ec6e05cf1efe8e95df0802c6f8e50572eafc1e3
tecliu <46478573+bubumumu@users.noreply.github.com>
2020-08-11 10:13:02 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,33 +26,4 @@ if [ ! -z $YAF_USE_SPL_AUTOLOAD ]; then
 	sed -i "s#^yaf\.use_spl_autoload=.*#yaf\.use_spl_autoload=${YAF_USE_SPL_AUTOLOAD}#g" /usr/local/etc/php/conf.d/yaf.ini
 fi
 
-if [ ! -f /xdebug_configured ]; then
-	if [ -z $XDEBUG_ENABLE ]; then
-		XDEBUG_ENABLE="On" 
-	fi
-	if [ -z $DOCKER_HOST_IP ]; then
-		DOCKER_HOST_IP="10.240.41.3"
-	fi
-	if [ -z $XDEBUG_PORT ]; then
-		XDEBUG_PORT=9001
-	fi
-	if [ -z $IDEKEY ]; then
-		IDEKEY="PHPSTORM"
-	fi
-    echo "=> Xdebug is not configured yet, configuring Xdebug ..."
-	echo "zend_extension=xdebug.so" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_enable=$XDEBUG_ENABLE" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_host=$DOCKER_HOST_IP" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_port=$XDEBUG_PORT" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_connect_back=On" >> /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_handler=dbgp" >> /usr/local/etc/php/conf.d/xdebug.ini
-	echo "xdebug.remote_autostart=On" >> /usr/local/etc/php/conf.d/xdebug.ini
-	echo "xdebug.profiler_enable=On" >> /usr/local/etc/php/conf.d/xdebug.ini
-	echo "xdebug.idekey=$IDEKEY" >> /usr/local/etc/php/conf.d/xdebug.ini
-	
-    touch /xdebug_configured
-else
-    echo "=> Xdebug is already configured"
-fi
-
 php-fpm
